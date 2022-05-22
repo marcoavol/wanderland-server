87e2df7839794d42eb7afbade2d383472ca57c07
ikeller <irene.keller@dbmr.unibe.ch>
2022-05-22 13:33:46 +0200

    initialized spring boot project

---
+++ src/main/java/com/wanderland/wanderlandserver/WanderlandServerApplication.java
@@ -0,0 +1,13 @@
+package com.wanderland.wanderlandserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderlandServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanderlandServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanderland/wanderlandserver/WanderlandServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderland.wanderlandserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderlandServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
