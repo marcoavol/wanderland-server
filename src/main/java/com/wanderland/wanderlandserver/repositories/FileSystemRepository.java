6c7c82ad6bcbc075491bdd3075e6967af8753074
Marco Volken <marco_volken@bluewin.ch>
2022-06-09 14:39:21 +0200

    Could fix path issue on windows

--- src/main/java/com/wanderland/wanderlandserver/repositories/FileSystemRepository.java
+++ src/main/java/com/wanderland/wanderlandserver/repositories/FileSystemRepository.java
@@ -3,16 +3,16 @@ package com.wanderland.wanderlandserver.repositories;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Repository;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Repository
 public class FileSystemRepository {
 
-    private final String RESOURCES_DIR = FileSystemRepository.class.getResource("/").getPath();
-
-    public Path saveInFileSystem(byte[] content, String fileName, String subdir) throws IOException {
-        Path filePath = Path.of(RESOURCES_DIR + subdir + "/" + fileName);
+    public Path saveInFileSystem(byte[] content, String fileName, String subdir) throws IOException, URISyntaxException {
+        Path classPath = Path.of(FileSystemRepository.class.getResource("/").toURI());
+        Path filePath = Path.of( classPath + subdir + "/" + fileName);
         Files.createDirectories(filePath.getParent());
         Files.write(filePath, content);
         return filePath;
