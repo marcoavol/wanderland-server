7ace9bcfed66cda0a58e25f559719111a130a542
ikeller <irene.keller@dbmr.unibe.ch>
2022-10-21 11:36:56 +0200

    add comment on how to renew certificate

--- renew_ssl_certificate.sh
+++ renew_ssl_certificate.sh
@@ -4,3 +4,4 @@ docker compose -f docker-compose-deploy.yml run --rm certbot renew
 # convert the certificate to PKCS12 format to use in backend
 cd /mnt/docker_images/certbot/conf/live/www.wanderland.dev
 openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name tomcat -CAfile chain.pem -caname root
+# In the last step, you will be prompted for a password. Make sure this matches the key-store-password in application-deploy.properties
